508abc56ca919f59331ae0f31df99c6c2390f3ca
V.Thanaledsumy <thanamvisva99@gmail.com>
2024-10-11 10:05:29 +0530

    Create Constractor 3_a

--- MovieRentalManagementSystem/Movie.cs
+++ MovieRentalManagementSystem/Movie.cs
@@ -6,11 +6,23 @@ using System.Threading.Tasks;
 
 namespace MovieRentalManagementSystem
 {
-    internal class Movie
+    public class Movie
     {
+      
+
         private string movieId {  get; set; }
         private string movieName { get; set; }
         private string movieUrl { get; set; }
-        private string rentalPrice
+        private string rentalPrice { get; set; }
+
+        public Movie(string movieId, string movieName, string movieUrl, string rentalPrice)
+        {
+            this.movieId = movieId;
+            this.movieName = movieName;
+            this.movieUrl = movieUrl;
+            this.rentalPrice = rentalPrice;
+        }
+
+
     }
 }
