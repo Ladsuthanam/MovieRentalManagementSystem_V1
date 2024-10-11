0ce0ec02629ba39e2f558c48932ec1f1984fe52e
V.Thanaledsumy <thanamvisva99@gmail.com>
2024-10-11 10:44:57 +0530

    q4_a,b

---
+++ MovieRentalManagementSystem/MovieManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRentalManagementSystem
+{
+    internal class MovieManager
+    {
+        private List<Movie> movies = new List<Movie>();
+        private string AutoM_Id = "MOVIE_001";
+        
+
+        public void CreateMovie(string movieId, string title, string directer, string rentalPrice)
+        {
+            var movie = new Movie(AutoM_Id,title,directer,rentalPrice);
+            movies.Add(movie);
+            Console.WriteLine("Movie added successfully");
+
+        }
+        public void ReadMovies()
+        {
+            if (movies == null) {
+                Console.WriteLine("Not Fount Movies");
+                return;
+            }
+            foreach (var movie in movies) {
+                Console.WriteLine("List of Movies:");
+                Console.WriteLine(movie);
+            }
+
+        }
+
+        public void UpdateMovie()
+        {
+
+        }
+        public void DeleteMovie()
+        {
+
+        }
+    }
+}
