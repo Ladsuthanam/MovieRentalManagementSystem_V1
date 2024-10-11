383bcf581bbe11f1d7878cb666eba9d7149243e2
UT01141 <UT01141@UNICOMTIC39>
2024-10-11 09:59:35 +0530

    Add project files.

---
+++ MovieRentalManagementSystem/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRentalManagementSystem
+{
+    internal class Movie
+    {
+        private string movieId {  get; set; }
+        private string movieName { get; set; }
+        private string movieUrl { get; set; }
+        private string rentalPrice
+    }
+}
