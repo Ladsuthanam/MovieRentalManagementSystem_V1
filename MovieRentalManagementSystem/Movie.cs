a1eccd8704aa6241a87c9fb5d0a544418b7133c4
V.Thanaledsumy <thanamvisva99@gmail.com>
2024-10-11 10:15:36 +0530

    q2

--- MovieRentalManagementSystem/Movie.cs
+++ MovieRentalManagementSystem/Movie.cs
@@ -10,24 +10,23 @@ namespace MovieRentalManagementSystem
 {
     public class Movie
     {
-      
 
         private string movieId {  get; set; }
         private string title { get; set; }
         private string directer {  get; set; }
         private string rentalPrice { get; set; }
 
-        public Movie(string movieId, string movieName, string movieUrl, string rentalPrice)
+
+
+        public Movie(string movieId, string title, string directer, string rentalPrice)
         {
-            MovieId = movieId;
-            Tittle = movieName;
-            movieUrl = movieUrl;
-            rentalPrice = rentalPrice;
+            this.movieId = movieId;
+            this.title = title;
+            this.directer = directer;
+            this.rentalPrice = rentalPrice;
         }
-        public override string ToString()
-        {
-            return $"movieIdId: {MovieId}, title: {Title}, director: {Director}, rentalPrice: {RentalPrice}";
-        }
+
+
 
     }
 }
